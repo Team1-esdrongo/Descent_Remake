196624aff6b496c435ecaca49431a93a347c67b4
Lodo Marco <marco.lodo1010@gmail.com>
2025-03-19 11:25:16 +0100

    Modify MainMenu & Added video on Play
    Changed img_background & buttons
    Created a new Scene
    Added video & image

--- Assets/Code/Script_Marco/MainMenu.cs
+++ Assets/Code/Script_Marco/MainMenu.cs
@@ -2,17 +2,35 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI title;
+    [SerializeField] private Button playBTN;
+    [SerializeField] private Button quitBTN;
+    private float lengthOfTime = 4.5f;
+
     public void OnPlayButton()
     {
-        SceneManager.LoadScene(1);
+        title.gameObject.SetActive(false);
+        playBTN.gameObject.SetActive(false);
+        quitBTN.gameObject.SetActive(false);
+        SceneManager.LoadScene("SCN_Loading");
+        //StartCoroutine(NextScene());
     }
 
     public void OnQuitButton()
     {
         Application.Quit();
         Debug.LogWarning("Game has been closed");
+    }
+
+    private IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(lengthOfTime);
+        SceneManager.LoadScene(1);
     }
 }
---
+++ Assets/Code/Script_Marco/VideoManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        videoPlayer.loopPointReached += VideoPlayer_loopPointReached;
+    }
+
+    private void VideoPlayer_loopPointReached(VideoPlayer source)
+    {
+        SceneManager.LoadScene("SCN_Marco");
+    }
+}
